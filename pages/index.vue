2075240b6a5ffdcbf0625e5d38c4019e5448748e
Anna-Sofija Sokolenko <mikafalkon8@gmail.com>
2023-05-22 00:36:53 +0300

    first commit

---
+++ pages/index.vue
@@ -0,0 +1,45 @@
+<template>
+    <h1 class="main-title">Get your daily dose of CATBOYS</h1>
+    <div class="box">
+        <div>
+            <img :src="images.url" alt="Catboy image" class="thumb">
+        </div>
+        <div>
+            <button class="btn" onClick="window.location.reload();">NEXT</button>
+        </div>
+    </div>
+</template>
+
+<script setup>
+    //fetch the images
+    const{ data: images } = await useFetch('https://api.catboys.com/img')
+    defineProps(['images'])
+</script>
+
+<style scoped>
+    .main-title{
+        text-align: center;
+    }
+    .thumb{
+        max-width: 50%;
+        max-height: 50%;
+        border: solid 1px black;
+    }
+    .box{
+        text-align: center;
+        display: flex;
+        justify-content: center;
+        flex-direction: column;
+    }
+    .btn{
+        max-width: 300px;
+        max-height: 100px;
+        padding: 1em;
+        text-align: center;
+        background-color: aquamarine;
+        font-size: 32px;
+        color: black;
+        border: 0px;
+        border-radius: 10%;
+    }
+</style>
